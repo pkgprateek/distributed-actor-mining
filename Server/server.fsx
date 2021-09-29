7a283992f66b6df10fce5baaceb84028119fc974
Prateek Kumar Goel <pkgprateek@gmail.com>
2021-09-29 01:06:42 -0400

    Added print actor, and functions for time and user input

--- Server/server.fsx
+++ Server/server.fsx
@@ -14,6 +14,12 @@ open Akka.Configuration
 open Akka.Remote
 open System.Diagnostics
 
+let mutable firstHash = 0
+let proc = Process.GetCurrentProcess()
+let cputimestamp = proc.TotalProcessorTime
+let timer = Stopwatch()
+timer.Start()
+
 
 // Creating the routing configuration
 let config = 
@@ -72,3 +78,46 @@ let strategy =
         | :? DivideByZeroException -> Directive.Resume
         | :? ArgumentException -> Directive.Stop
         | _ -> Directive.Escalate)
+
+
+// Printer actor to print the found string
+let printActor (mailbox : Actor<_>) =
+    let rec messageLoop() =
+        actor {
+            let! msg = mailbox.Receive()
+            match msg with
+            | Final(hashIn, hashOut) ->
+                printfn "%s\t%s\n" hashIn hashOut
+                if firstHash = 0 then
+                    firstHash <- firstHash + 1
+                    let cputime = (proc.TotalProcessorTime - cputimestamp).TotalMilliseconds
+                    printfn "CPU time = %dms" (int64 cputime)
+                    printfn "Absolute time = %dms\n\n" timer.ElapsedMilliseconds
+                    timer.Stop()
+            | _ -> ()
+            return! messageLoop()
+        }
+    messageLoop()
+// Creating a reference for Printer Actor
+let printRef = spawn system "ServerPrinter" printActor
+
+
+
+
+
+// Taking user input from Command Line
+let userInput = System.Int32.Parse(Environment.GetCommandLineArgs().[2])
+
+// Get Input
+let sendUserInput (mailbox:Actor<_>) =
+    let rec loop () = actor {
+        let! message = mailbox.Receive()
+        mailbox.Sender() <! userInput
+    }
+    loop ()
+let serveInputRef = spawn system "inputserver" sendUserInput
+
+
+// Wait until all the actors has finished processing
+// system.WhenTerminated.Wait()
+Console.ReadLine() |> ignore
